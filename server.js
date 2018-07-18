cef1938285d9df02372839ccc35fd1a1d0259987
tsirlucas <tsirlucas@gmail.com>
2018-07-18 17:14:58 -0300

    fix(pgweb): fix redirects for pgweb files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const httpProxy = require('express-http-proxy');
+const {conditionalMapper} = require('./util');
 
 require('dotenv').config();
 
@@ -9,12 +10,7 @@ const authServiceProxy = httpProxy(process.env.AUTH_API_ENDPOINT);
 const dataServiceProxy = httpProxy(process.env.DATA_API_ENDPOINT);
 const streamServiceProxy = httpProxy(process.env.STREAM_API_ENDPOINT);
 const dbProxy = httpProxy(process.env.DATABASE_ENDPOINT);
-const pgwebServiceProxy = httpProxy(process.env.PGWEB_ENDPOINT, {
-  pathRewrite: {
-      '^/static/' : '/pgweb/static'      // rewrite path
-  }
-});
-
+const pgwebServiceProxy = httpProxy(process.env.PGWEB_ENDPOINT)
 
 // Authentication
 app.use((req, res, next) => {
@@ -31,6 +27,8 @@ app.use('/stream', streamServiceProxy)
 app.use('/db', dbProxy)
 app.use('/pgweb', pgwebServiceProxy)
 
+app.use('/static', conditionalMapper(/(pgweb|font-awesome.css)/, '/static'))
+app.use('/api', conditionalMapper('pgweb', '/api'))
 
 const httpServer = app.listen(process.env.PORT || 8080, (error) => {
   if (error) {
