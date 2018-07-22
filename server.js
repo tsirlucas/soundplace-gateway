5cc39f381ab6111ce5b9428e534acb7c500a5096
tsirlucas <tsirlucas@gmail.com>
2018-07-22 17:17:21 -0300

    feat(graphql): add graphql endpoint

--- server.js
+++ server.js
@@ -6,22 +6,32 @@ require('dotenv').config();
 
 const app = express();
 
+const changePath = (req) => {
+  const parts = req.url.split('?');
+  const queryString = parts[1];
+  const updatedPath = parts[0] + 'graphql';
+  const resolvedPathValue = updatedPath + (queryString ? '?' + queryString : '');
+  return resolvedPathValue;
+}
+
 const authServiceProxy = httpProxy(process.env.AUTH_API_ENDPOINT);
 const dataServiceProxy = httpProxy(process.env.DATA_API_ENDPOINT);
 const streamServiceProxy = httpProxy(process.env.STREAM_API_ENDPOINT);
-
+const graphqlServiceProxy = httpProxy(process.env.GRAPHQL_API_ENDPOINT, {
+  proxyReqPathResolver: (req) => changePath(req, 'graphql'),
+});
 
 app.get('/', (req, res) => res.send('Working :D'))
-
-app.use('/auth', authServiceProxy)
-app.use('/data', dataServiceProxy)
-app.use('/stream', streamServiceProxy)
+   .use('/auth', authServiceProxy)
+   .use('/data', dataServiceProxy)
+   .use('/stream', streamServiceProxy)
+   .use('/graphql', graphqlServiceProxy)
 
 const httpServer = app.listen(process.env.PORT || 8080, (error) => {
   if (error) {
-      console.error(error);
+    console.error(error);
   } else {
-      const address = httpServer.address();
-      console.info(`==> 🌎 Listening on ${address.port}. Open up http://localhost:${address.port}/ in your browser.`);
+    const address = httpServer.address();
+    console.info(`==> 🌎 Listening on ${address.port}. Open up http://localhost:${address.port}/ in your browser.`);
   }
 });
